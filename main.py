bdee6a8b76b0a197f4091fab58683a336f387392
R S <Rei2622Engineer@gmail.com>
2025-06-19 14:15:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 app= FastAPI()
+class Item(BaseModel):
+    name:str
+    price: float
+    is_offer: bool=False
+
 @app.get("/")
 def read_root():
     return{"message":"hello,Fastapi!"}
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str=None):
-    return{"item_id": item_id, "query":q}
+
+@app.post("/items/")
+def create_item(item: Item):
+    return{
+        "name": item.name,
+        "price":item.price,
+        "is_offer":  item.is_offer
+    }
+
